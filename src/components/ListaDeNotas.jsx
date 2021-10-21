88aad45685bf10b80a5b35e034f67d78a857e6f3
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-21 06:36:06 -0300

    Aula 2: Finalizada!

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import CardNote from "./CardNote";
+
+class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNote />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
